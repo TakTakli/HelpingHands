b93653e6b0eb74a40ac29d9a93af78b7f0cce999
Alvi Binte Zamil <alvi.binte.zamil@gmail.com>
2025-02-10 22:38:02 +0600

    Sorted file directory

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,15 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+//import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root= FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Parent root= FXMLLoader.load(getClass().getResource("/resources/fxml/Main.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/resources/css/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
