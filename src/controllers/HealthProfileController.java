967d472ad8f1e817b6c5a52ad96012c1f5219712
Alvi Binte Zamil <alvi.binte.zamil@gmail.com>
2025-02-14 21:27:33 +0600

    Created UI for Health Profile Scene

--- src/controllers/HealthProfileController.java
+++ src/controllers/HealthProfileController.java
@@ -2,8 +2,24 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 public class HealthProfileController {
+	@FXML private TextField fname=new TextField();
+	@FXML private TextField lname=new TextField();
+	@FXML private TextField phone=new TextField();
+	@FXML private TextField mail=new TextField();
+	@FXML private TextField otherdis=new TextField();
+	@FXML private TextField otherallergy=new TextField();
 	
+	void getInputFromField()
+	{
+	
+	}
+	
+	void getSelectedCheckBox()
+	{
+		
+	}
 }
