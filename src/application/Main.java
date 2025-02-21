6548acfbaa5b4f84d43ef857b96f1c9483282352
Than <thanmumu13@gmail.com>
2025-02-21 13:51:55 +0600

    login_signup

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,21 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-//import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root= FXMLLoader.load(getClass().getResource("/controllers/OnboardingScreen1.fxml"));
-			Scene scene = new Scene(root, Color.WHITE);
-			scene.getStylesheets().add(getClass().getResource("/resources/css/onboarding.css").toExternalForm());
+			Parent root=FXMLLoader.load(getClass().getResource("sample.fxml"));
+			Scene scene = new Scene(root,600,600);
+			primaryStage.setTitle("HelpingHands");
 			primaryStage.setScene(scene);
-//			primaryStage.setFullScreen(true);
-			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/resources/icons/helpinghands_logo.png")));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
